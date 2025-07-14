97eb3798ef3c0bf6578b6e5f06cea3339121a064
Victor Oliveira <victor.oliveira20@icloud.com>
2025-07-14 09:43:45 -0300

    project created

---
+++ src/test/java/com/victor_oliveira/qrcode/generator/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor_oliveira.qrcode.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
